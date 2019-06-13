fdfd68e277fcfca02e18cb73faf0dbcfedcfc56b
Camilo Castro <ccspadafora@gmail.com>
2019-06-13 09:35:38 -0500

    Se incluyen los cambios al header de fixit en html y css

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,20 +5,44 @@ import './styles/Header.css';
 
 class Header extends React.Component {
     render () {
-        return (
-        <div className="header">
-            <Link to='/'>
-                <img src="" alt="LogoFixIt"/>
-            </Link>
-            <Link to='/admincliente'> <h4>admincliente</h4></Link>
-            <Link to='/adminfixit'> <h4>adminfixit</h4></Link>
-            <Link to='/login'> <h4>login</h4></Link>
-            <Link to='/soporte'> <h4>soporte</h4></Link>
-            <Link to='/thankyou'> <h4>thankyou</h4></Link>
-            <Link to='/trabajo'> <h4>trabajo</h4></Link>
-        </div>
-      );
+    return (
+    <header className='header'>
+      <div className='container-header'>
+       <figure className='logo-fixit-header'>
+       <img src='../' alt="LogoFixIt"/>
+       </figure>
+       <nav className='menu-principal'>
+           <ul>
+               <li>
+                   <a href='#admincliente'>Trabaja con FixIt</a>
+               </li>
+               <li>
+                   <a href='#adminfix'>Soporte</a>
+               </li>
+               <li>
+                   <a href='#login'>mi cuenta</a>
+               </li>
+               <li>
+                   <a href='#arreglalo ya'>arreglalo ya</a>
+               </li>
+           </ul>
+       </nav>
+      </div>
+    </header>
+    );
+      // <div className="header">
+      //     <Link to='/'>
+      //         <img src="" alt="LogoFixIt"/>
+      //     </Link>
+      //     <Link to='/admincliente'> <h4>admincliente</h4></Link>
+      //     <Link to='/adminfixit'> <h4>adminfixit</h4></Link>
+      //     <Link to='/login'> <h4>login</h4></Link>
+      //     <Link to='/soporte'> <h4>soporte</h4></Link>
+      //     <Link to='/thankyou'> <h4>thankyous</h4></Link>
+      //     <Link to='/trabajo'> <h4>trabajo</h4></Link>
+      // </div>
     }
 }
 
 export default Header;
+
