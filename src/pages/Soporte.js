72c605baad47ccc7659443be6bd66232608d0559
Camilo Castro <ccspadafora@gmail.com>
2019-07-11 15:42:31 -0500

    se agregan modificaciones estéticas a la página de sorporte

--- src/pages/Soporte.js
+++ src/pages/Soporte.js
@@ -2,15 +2,19 @@ import React, {Component} from 'react';
 import './styles/Soporte.css';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-
+import Backgroundsoporte1 from '../images/soporteimg1.jpg';
+import Backgroundsoporte2 from '../images/soporteimg2.jpeg';
+import Backgroundsoporte3 from '../images/soporteimg3.jpg';
 
 class Soporte extends Component {
     render () {
         return (
         <React.Fragment>
         <Header/>
+
+{/* INICIO DE ACORDOEÓN DE PREGUNTAS FRECUENTES */}
         <div className="acordeon-container">
-            <h1 className="titulo-acordeon">Preguntas </h1>
+            <h1 className="titulo-acordeon">Preguntas Frecuentes </h1>
             <div className="acordeon">
               <div className="acordeon-item">
                 <input type="radio" name="acordeon" id="item1"></input>
@@ -40,43 +44,84 @@ class Soporte extends Component {
               </div>
             </div>
             <div>
+{/* FIN DE ACORDEÓN DE PREGUNTAS FRECUENTES   */}
+
+{/* INICIO DE SECCIÓN NECESITAS AYUDA? */}
                 <form action="" className="formulario-soporte">
-                 <h3>Necesitas ayuda?</h3>
-                 <p>Nos pondremos en contacto contigo</p>
-                 <input type="text" placeholder="Nombre y apellido" className="e-mail"></input>
-                 <input type="text" placeholder="Correo electrónico" className="e-mail"></input>
-                 <input type="phone" placeholder="Número de contacto" className="e-mail"></input>
+                  <h3>¿Necesitas ayuda?</h3>
+                  <p>Nos pondremos en </p>
+                  <p>contacto contigo</p>
+                <div className='form-container'>
+                    <label for='nya'>Nombre y apellido</label>
+                    <label for='email'>Correo electrónico</label>
+                    <label for='phone'>Número de contacto</label>
+                  <div className='campos-formulario-soporte'>
+                    <input id='nya' type="text" className="e-mail"></input>
+                    <input id='email' type="text" className="e-mail"></input>
+                    <input id='phone' type="phone" className="e-mail"></input>
+                  </div>
                  <button className="boton-formulario">Enviar</button>
+                 </div>
                 </form>
+ {/* FIN DE SECCIÓN NECESITAS AYUDA?*/}
+
+ {/* INICIO SECCIÓN "UN SERVICIO AMIGABLE Y DE CALIDAD" */}
             </div> 
             <div className="seccion-informacion-soporte">
               <section className="grid1-soporte">
-                <div className="item-grid1-soporte">
-                  <figure className="soporte-imagewrapper"> 
-                    <img className="soporte-image1" src="" alt="imagen1"></img>
-                  </figure>
+                <div className="item-grid1-soporte" style={{
+                     backgroundImage: "url(" + Backgroundsoporte1 + ")",
+                     backgroundSize: 'cover',
+                     backgroundPositionX:'center',
+                     backgroundPositionY:'center',
+                     // filter: 'brightness(0.5)',
+                     KhtmlOpacity:'0.5',
+                     MozOpacity:'0.5',
+                     msFilter:'alpha(opacity=50)',
+                     filter:'alpha(opacity=50)',
+                     filter:'progid:DXImageTransform.Microsoft.Alpha(opacity=0.5)',
+                     // opacity:'0.7',
+                  }}>
                 </div>
                 <div className="item-grid1-soporte">
                  <h2 className="soporte-subtitulo-2">Un servicio amigable y de calidad!</h2>
                 </div>
-                <div className="item-grid1-soporte">
-                  <figure className="soporte-imagewrapper"> 
-                    <img className="soporte-image1" src="" alt="imagen1"></img>
-                  </figure>
+                <div className="item-grid1-soporte" style={{
+                     backgroundImage: "url(" + Backgroundsoporte2 + ")",
+                     backgroundSize: 'cover',
+                     // filter: 'brightness(0.5)',
+                     KhtmlOpacity:'0.5',
+                     MozOpacity:'0.5',
+                     msFilter:'alpha(opacity=50)',
+                     filter:'alpha(opacity=50)',
+                     filter:'progid:DXImageTransform.Microsoft.Alpha(opacity=0.5)',
+                     // opacity:'0.7',
+                  }}>
                 </div>
                 <div className="item-grid1-soporte">
                   <p>Aliquet eget sit. Id diam maecenas ultricies mi Rhoncus aenean. Aliquet eget sit. Id diam maecenas ultricies mi Rhoncus aenean. Aliquet eget sit. Id diam maecenas ultricies mi Rhoncus aenean. Aliquet eget sit. Id diam maecenas ultricies mi Rhoncus aenean</p>
                 </div>
-                <div className="item-grid1-soporte">
-                  <figure className="soporte-imagewrapper"> 
-                    <img className="soporte-image1" src="" alt="imagen1"></img>
-                  </figure>
+                <div className="item-grid1-soporte" style={{
+                     backgroundImage: "url(" + Backgroundsoporte3 + ")",
+                     backgroundSize: 'cover',
+                     backgroundPositionY:'center',
+                     backgroundPositionX:'center',
+                    //  backgroundPositionX:'left',
+                     // filter: 'brightness(0.5)',
+                     KhtmlOpacity:'0.5',
+                     MozOpacity:'0.5',
+                     msFilter:'alpha(opacity=50)',
+                     filter:'alpha(opacity=50)',
+                     filter:'progid:DXImageTransform.Microsoft.Alpha(opacity=0.5)',
+                     // opacity:'0.7',
+                  }}>
                 </div>
                 <div className="item-grid1-soporte">
-                 <button type="submit">Whatsapp</button>
+                 <button type="submit" className='wp-button'>Whatsapp</button>
                 </div>
               </section>
             </div>
+ {/* FIN DE SECCIÓN "UN SERVICIO AMIGABLE Y DE CALIDAD"*/}
         </div>
         <Footer/>
         </React.Fragment>
