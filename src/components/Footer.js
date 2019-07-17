a0c4b67119d88fce991afa447e8ae7bd5dcc5977
Camilo Castro <ccspadafora@gmail.com>
2019-07-17 13:01:02 -0500

    se agregan estilos al footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,34 @@
 import React, {Component} from 'react';
-
+import logofixit from '../images/logofix-it.png';
 import './styles/Footer.css';
 
 class Footer extends Component {
     render () {
         return (
-        <div className="footer">
-            <h3>Título 1</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eget libero tincidunt, fringilla enim vel, tristique nisl. Lorem ipsum dolor sit amet.</p>
-            <h3>Título 2</h3>
-            <p>Lorem ipsumo dolor sit amet, consectetur adipiscing elit. Praesent eget libero tincidunt, fringilla enim vel, tristique nisl. Lorem ipsum dolor sit amet.</p>
-            <h3>Título 3</h3>
-            <p>Loremo ipsum dolor sit amet, consectetur adipiscing elit. Praesent eget libero tincidunt, fringilla enim vel, tristique nisl. Lorem ipsum dolor sit amet.</p>
-        </div>
+<div className="footer">
+    <div className='first-footer'>
+        <div className='info-containter-footer'>  
+           <div className='itemfooter'>
+             <h3>Acerca de nosotros</h3>
+             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eget libero tincidunt, fringilla enim vel, tristique nisl. Lorem ipsum dolor sit amet.</p>
+           </div>
+           <div className='itemfooter'>
+             <h3>Trabaja con nosotros</h3>
+             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eget libero tincidunt, fringilla enim vel, tristique nisl. Lorem ipsum dolor sit amet.</p>
+           </div>
+           <div className='itemfooter'>
+             <h3>Contacto</h3>
+             <p>Bogotá, Colombia.</p>
+             <p>Calle xx # xx - xx</p>
+             <p>xxx xxxx xxx</p>
+             <p>xxxxxx@mail.com</p>
+           </div>
+          </div> 
+          <div className='footer-img-container'><img src={logofixit} alt='logofixit'></img></div>   
+    </div>
+    <div className='second-footer'><h6>Desarrollo web por Sense Digital</h6></div>
+</div>
+     
       );
     }
 }
